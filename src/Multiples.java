0043bad7bcc02a6c1e39bbb87bac401ea4abc2ad
hoooing <kdh010125@gmail.com>
2023-09-15 11:49:24 -0400

    Implemented Multiples function to check multiples of 3 or 5 < 1000

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Multiples {
+public class Multiples {
+    public static void main(String[] args) {
+        int limit = 1000;
+        int count = 0;
+
+        for (int i = 1; i < limit; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                count++;
+            }
+        }
+
+        System.out.println("The number of multiples of 3 or 5 below 1000 is: " + count);
+    }
 }
